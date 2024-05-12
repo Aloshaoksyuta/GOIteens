bbdbc73bc2002add525f26297dbca0b95c73ec1c
Aloshaoksyuta <oksutaoleksij@gmail.com>
2024-05-12 11:17:05 +0300

    my first commit

---
+++ goiteens.py
@@ -0,0 +1,10 @@
+
+from statistics import mean
+
+math_scores = {
+    "yaroslav": [8, 7, 9, 6, 8],
+    "kevin": [7, 6, 5, 4, 3],
+    "oleksiy": [8, 7, 9, 6, 10],
+    "oleh": [10, 9, 8, 10, 10],
+    "andrii": [6, 7, 8, 9, 6]
+}
