166659228f3dc7864b33bd39be8d69f9bf42ad7f
Aloshaoksyuta <oksutaoleksij@gmail.com>
2024-05-12 11:22:22 +0300

    add count lines

--- goiteens.py
+++ goiteens.py
@@ -1,10 +1,10 @@
-
-from statistics import mean
-
-math_scores = {
-    "yaroslav": [8, 7, 9, 6, 8],
-    "kevin": [7, 6, 5, 4, 3],
-    "oleksiy": [8, 7, 9, 6, 10],
-    "oleh": [10, 9, 8, 10, 10],
-    "andrii": [6, 7, 8, 9, 6]
-}
+def count_lines(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            line_count = 0
+            for _ in file:
+                line_count += 1
+            return line_count
+    except FileNotFoundError:
+        print(f"Файл '{file_path}' не знайдено.")
+        return -1
